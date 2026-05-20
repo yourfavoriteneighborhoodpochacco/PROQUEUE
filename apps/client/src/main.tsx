0d74d35e19eee8073e36f9d770bef8b9c5bf6ef0
mponiente <michaelwrites.biz@gmail.com>
2026-05-19 22:15:33 -0700

    added vercel analytics

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Home } from './pages/Home';
 import { PlayerProfile } from './features/player/PlayerProfile';
 import './styles/global.css';
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/player/:gameName/:tagLine" element={<PlayerProfile />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>
 );
